d110f7e53f8d2194dfb440d28a493188e321a37a
kwohl <ktwohl@gmail.com>
2020-06-16 12:27:46 -0500

    built out login and register

--- domino_api/urls.py
+++ domino_api/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from dominoapi.models import *
-from dominoapi.views import TaskLists, Tasks, Steps, TaskSteps, Users
+from dominoapi.views import TaskLists, Tasks, Steps, TaskSteps, Users, register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'taskLists', TaskLists, 'list')
@@ -30,6 +30,8 @@ router.register(r'users', Users, 'user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', register_user),
+    path('login/', login_user),
     path('api-token-auth/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
