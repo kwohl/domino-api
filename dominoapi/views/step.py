dca2d39b5d489d5f4916736f8d54d298a248a9ea
kwohl <ktwohl@gmail.com>
2020-06-21 16:54:17 -0500

    added destroy method in step view

--- dominoapi/views/step.py
+++ dominoapi/views/step.py
@@ -72,3 +72,21 @@ class Steps(ViewSet):
         )
 
         return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single step
+        Returns:
+            Response -- 200, 404, or 500 status code 
+        """
+
+        try:
+            step = Step.objects.get(pk=pk)
+            step.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Step.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+            
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
