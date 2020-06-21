7df387a317f0aa7613612754963aeff84ad095d9
kwohl <ktwohl@gmail.com>
2020-06-21 15:18:57 -0500

    initial create method for taskSteps

--- dominoapi/views/taskstep.py
+++ dominoapi/views/taskstep.py
@@ -53,3 +53,50 @@ class TaskSteps(ViewSet):
             context={'request': request}
         )
         return Response(serializer.data)
+
+    
+    # def create(self, request):
+        
+    #     try:
+    #         step = Step.objects.get(pk=request.data['step_id'])
+            
+    #         new_task_step = TaskStep()
+    #         new_task_step.task_id = request.data['task_id']
+    #         new_task_step.step_id = step.id
+            
+    #         new_task_step.save()
+
+    #         serialize = TaskStepSerializer(new_order_product, context={'request': request}) 
+    #     except:
+    #         new_step = Step()
+    #         new_step.name = request.data['step_name']
+    #         new_step.description = request.data['step_description']
+    #         new_step.is_complete = False
+    #         new_step.importance = None
+    #         new_step.save()
+
+    #         new_task_step = TaskStep()
+    #         new_task_step.task_id = request.data['task_id']
+    #         new_task_step.step_id = new_step.id
+
+    #         new_order_product.save()
+
+    #         serialize = TaskStepSerializer(new_order_product, context={'request': request}) 
+
+    #     return Response(serialize.data) 
+    
+    def create(self, request):
+        task = Task.objects.get(pk=request.data["task_id"])
+        step = Step.objects.get(pk=request.data["step_id"])
+
+        new_task_step = TaskStep()
+        new_task_step.task = task 
+        new_task_step.step = step
+        
+        new_task_step.save()
+
+        serializer = TaskStepSerializer(
+            new_task_step, context={'request': request}
+        )
+
+        return Response(serializer.data)
